eb5ae4df2f16a254d83b51ce03ebb7ce9291b385
erichiroshi <erichiroshi@gmail.com>
2020-10-16 14:15:05 -0400

    3.8. Consultando objetos do banco de dados

---
+++ src/main/java/com/erichiroshi/algafood/jpa/CadastroCozinha.java
@@ -0,0 +1,22 @@
+package com.erichiroshi.algafood.jpa;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Component;
+
+import com.erichiroshi.algafood.domain.model.Cozinha;
+
+@Component
+public class CadastroCozinha {
+
+	@PersistenceContext
+	private EntityManager manager;
+
+	public List<Cozinha> listar() {
+		return manager.createQuery("from Cozinha", Cozinha.class).getResultList();
+	}
+
+}
