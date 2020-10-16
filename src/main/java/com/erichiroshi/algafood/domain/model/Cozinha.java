5b7e0042f75c8b473fbf0ff891bbfbc1cbd4ad7a
erichiroshi <erichiroshi@gmail.com>
2020-10-16 13:37:19 -0400

    3.5. Criando as tabelas do banco a partir das entidades

--- src/main/java/com/erichiroshi/algafood/domain/model/Cozinha.java
+++ src/main/java/com/erichiroshi/algafood/domain/model/Cozinha.java
@@ -3,8 +3,10 @@ package com.erichiroshi.algafood.domain.model;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "tab_cozinha")
 public class Cozinha {
 
 	@Id
