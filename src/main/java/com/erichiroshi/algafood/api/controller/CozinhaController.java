383203c61ef8c11abe94ca31809721524aad1937
erichiroshi <erichiroshi@gmail.com>
2020-10-20 16:30:55 -0400

    4.16. Customizando a representação em XML com Wrapper e anotações do Jackson

--- src/main/java/com/erichiroshi/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/erichiroshi/algafood/api/controller/CozinhaController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.erichiroshi.algafood.api.model.CozinhasXmlWrapper;
 import com.erichiroshi.algafood.domain.model.Cozinha;
 import com.erichiroshi.algafood.domain.repository.CozinhaRepository;
 
@@ -19,16 +20,14 @@ public class CozinhaController {
 	@Autowired
 	private CozinhaRepository cozinhaRepository;
 
-	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-	public List<Cozinha> listar1() {
-		System.out.println("Listar 1");
+	@GetMapping
+	public List<Cozinha> listar() {
 		return cozinhaRepository.listar();
 	}
 	
 	@GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
-	public List<Cozinha> listar2() {
-		System.out.println("Listar 2");
-		return cozinhaRepository.listar();
+	public CozinhasXmlWrapper listarXml() {
+		return new CozinhasXmlWrapper(cozinhaRepository.listar());
 	}
 	
 	@GetMapping("/{cozinhaId}")
--- src/main/java/com/erichiroshi/algafood/domain/model/Cozinha.java
+++ src/main/java/com/erichiroshi/algafood/domain/model/Cozinha.java
@@ -6,13 +6,12 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonRootName;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-@JsonRootName(value = "gastronomia")
+@JsonRootName(value = "cozinha")
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
@@ -23,8 +22,6 @@ public class Cozinha {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-//	@JsonIgnore
-	@JsonProperty(value = "titulo")
 	@Column(nullable = false)
 	private String nome;
 
