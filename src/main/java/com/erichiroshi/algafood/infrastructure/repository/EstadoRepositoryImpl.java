0f03b25067f71379994f333e96ab482e62ae2bc6
erichiroshi <erichiroshi@gmail.com>
2020-10-20 14:00:08 -0400

    4.11. Desafio: collection resource de estados

---
+++ src/main/java/com/erichiroshi/algafood/infrastructure/repository/EstadoRepositoryImpl.java
@@ -0,0 +1,43 @@
+package com.erichiroshi.algafood.infrastructure.repository;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Component;
+
+import com.erichiroshi.algafood.domain.model.Estado;
+import com.erichiroshi.algafood.domain.repository.EstadoRepository;
+
+@Component
+public class EstadoRepositoryImpl implements EstadoRepository {
+
+	@PersistenceContext
+	private EntityManager manager;
+
+	@Override
+	public List<Estado> listar() {
+		return manager.createQuery("from Estado", Estado.class).getResultList();
+	}
+
+	@Override
+	public Estado buscar(Long id) {
+		return manager.find(Estado.class, id);
+	}
+
+	@Transactional
+	@Override
+	public Estado salvar(Estado estado) {
+		return manager.merge(estado);
+	}
+
+	@Transactional
+	@Override
+	public void remover(Estado estado) {
+		estado = buscar(estado.getId());
+		manager.remove(estado);
+	}
+
+}
