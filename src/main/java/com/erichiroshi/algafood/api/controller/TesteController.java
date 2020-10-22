350c97d17779aaeda386de79a70c0320b9a3be4d
erichiroshi <erichiroshi@gmail.com>
2020-10-22 13:50:31 -0400

    5.1. Implementando consultas JPQL em repositórios

---
+++ src/main/java/com/erichiroshi/algafood/api/controller/TesteController.java
@@ -0,0 +1,26 @@
+package com.erichiroshi.algafood.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.erichiroshi.algafood.domain.model.Cozinha;
+import com.erichiroshi.algafood.domain.repository.CozinhaRepository;
+
+@RestController
+@RequestMapping("/teste")
+public class TesteController {
+
+	@Autowired
+	private CozinhaRepository cozinhaRepository;
+	
+	@GetMapping(value = "/cozinhas/por-nome")
+	public List<Cozinha> cozinhasPorNome(@RequestParam(value = "nome") String nome){
+		return cozinhaRepository.consultarPorNome(nome);
+		
+	}
+}
