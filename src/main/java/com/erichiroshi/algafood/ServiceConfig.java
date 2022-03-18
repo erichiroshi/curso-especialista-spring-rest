d98fcd4ec11f2a6b0bf893f9ba98e9cd156a3dba
erich <erich@DESKTOP-HUIJ7D8>
2022-03-18 14:16:59 -0400

    2.21. Criando métodos de callback do ciclo de vida dos beans

---
+++ src/main/java/com/erichiroshi/algafood/ServiceConfig.java
@@ -0,0 +1,15 @@
+package com.erichiroshi.algafood;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.erichiroshi.algafood.di.service.AtivacaoClienteService;
+
+@Configuration
+public class ServiceConfig {
+
+	@Bean(initMethod = "init", destroyMethod = "destroy")
+	public AtivacaoClienteService ativacaoClienteService() {
+		return new AtivacaoClienteService();
+	}
+}
--- src/main/java/com/erichiroshi/algafood/di/service/AtivacaoClienteService.java
+++ src/main/java/com/erichiroshi/algafood/di/service/AtivacaoClienteService.java
@@ -1,19 +1,32 @@
 package com.erichiroshi.algafood.di.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
 import com.erichiroshi.algafood.di.modelo.Cliente;
 import com.erichiroshi.algafood.di.notificacao.NivelUrgencia;
 import com.erichiroshi.algafood.di.notificacao.Notificador;
 import com.erichiroshi.algafood.di.notificacao.TipoDoNotificador;
 
-@Component
+//@Component
 public class AtivacaoClienteService {
 
 	@TipoDoNotificador(NivelUrgencia.SEM_URGENCIA)
 	@Autowired
 	private Notificador notificador;
+
+	public AtivacaoClienteService() {
+		System.out.println("Ativação Cliente Service");
+	}
+
+//	@PostConstruct
+	private void init() {
+		System.out.println("init");
+	}
+
+//	@PreDestroy
+	private void destroy() {
+		System.out.println("destroy");
+	}
 
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
