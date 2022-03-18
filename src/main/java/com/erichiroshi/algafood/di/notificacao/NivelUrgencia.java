4802a92f32d70b3652dbe1e2c72f661dfd0e8bf3
erich <erich@DESKTOP-HUIJ7D8>
2022-03-18 17:41:51 -0400

    3.3. Adicionando JPA e configurando o Data Source

--- src/main/java/com/erichiroshi/algafood/di/notificacao/NivelUrgencia.java
+++
@@ -1,7 +0,0 @@
-package com.erichiroshi.algafood.di.notificacao;
-
-public enum NivelUrgencia {
-
-	URGENTE,
-	SEM_URGENCIA;
-}
--- src/main/java/com/erichiroshi/algafood/di/notificacao/Notificador.java
+++
@@ -1,9 +0,0 @@
-package com.erichiroshi.algafood.di.notificacao;
-
-import com.erichiroshi.algafood.di.modelo.Cliente;
-
-public interface Notificador {
-
-	void notificar(Cliente cliente, String mensagem);
-
-}
