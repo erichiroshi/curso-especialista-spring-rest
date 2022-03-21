7e743ea2b9d3b16dcc7e65a1e9d07132a04f72c7
erich <erich@DESKTOP-HUIJ7D8>
2022-03-21 09:42:20 -0400

    3.12. Excluindo um objeto do banco de dados

--- src/main/java/com/erichiroshi/algafood/jpa/CadastroCozinha.java
+++ src/main/java/com/erichiroshi/algafood/jpa/CadastroCozinha.java
@@ -30,4 +30,10 @@ public class CadastroCozinha {
 		return entityManager.merge(cozinha);
 	}
 	
+	@Transactional
+	public void remover(Cozinha cozinha) {
+		cozinha = buscar(cozinha.getId());
+		entityManager.remove(cozinha);
+	}
+	
 }
---
+++ src/main/java/com/erichiroshi/algafood/jpa/ExclusaoCozinhaMain.java
@@ -0,0 +1,25 @@
+package com.erichiroshi.algafood.jpa;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import com.erichiroshi.algafood.AlgafoodApiApplication;
+import com.erichiroshi.algafood.domain.model.Cozinha;
+
+public class ExclusaoCozinhaMain {
+
+	public static void main(String[] args) {
+		ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
+				.web(WebApplicationType.NONE)
+				.run(args);
+
+		CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+
+		Cozinha cozinha = new Cozinha();
+		cozinha.setId(1L);
+		
+		cadastroCozinha.remover(cozinha);
+
+	}
+}
