a218d44107596e307bba25bb842fbf0ce716d0a1
erich <erich@DESKTOP-HUIJ7D8>
2022-03-21 14:12:11 -0400

    3.17. Mapeando relacionamento com @ManyToOne

--- src/main/java/com/erichiroshi/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/erichiroshi/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -5,11 +5,13 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.erichiroshi.algafood.domain.model.Restaurante;
 import com.erichiroshi.algafood.domain.repository.RestauranteRepository;
 
+@Component
 public class RestauranteRepositoryImpl implements RestauranteRepository {
 
 	@PersistenceContext
