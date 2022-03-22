974349c54207f11c9f873c48b0ab0309b97f35a1
erich <erich@DESKTOP-HUIJ7D8>
2022-03-22 15:31:08 -0400

    4.23. Modelando e implementando a inclusão de recursos com POST

--- src/main/java/com/erichiroshi/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/erichiroshi/algafood/api/controller/CozinhaController.java
@@ -1,5 +1,6 @@
 package com.erichiroshi.algafood.api.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,8 +8,11 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.erichiroshi.algafood.api.model.CozinhasXmlWrapper;
 import com.erichiroshi.algafood.domain.model.Cozinha;
@@ -37,9 +41,14 @@ public class CozinhaController {
 
 		if (cozinha != null)
 			return ResponseEntity.ok(cozinha);
-
-//		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 		return ResponseEntity.notFound().build();
+	}
+
+	@PostMapping
+	public ResponseEntity<Cozinha> adicionar(@RequestBody Cozinha cozinha) {
+		cozinha = cozinhaRepository.salvar(cozinha);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(cozinha.getId()).toUri();
+		return ResponseEntity.created(uri).body(cozinha);
 	}
 
 }
