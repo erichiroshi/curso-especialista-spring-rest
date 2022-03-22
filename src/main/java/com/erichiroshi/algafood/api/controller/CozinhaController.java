ccc48736c4684aae72fb6ad2c482fa67ced46320
erich <erich@DESKTOP-HUIJ7D8>
2022-03-22 14:23:47 -0400

    4.16. Customizando a representação em XML com Wrapper e anotações do Jackson

--- src/main/java/com/erichiroshi/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/erichiroshi/algafood/api/controller/CozinhaController.java
@@ -3,11 +3,13 @@ package com.erichiroshi.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.erichiroshi.algafood.api.model.CozinhasXmlWrapper;
 import com.erichiroshi.algafood.domain.model.Cozinha;
 import com.erichiroshi.algafood.domain.repository.CozinhaRepository;
 
@@ -21,6 +23,11 @@ public class CozinhaController {
 	@GetMapping
 	public List<Cozinha> listar1() {
 		return cozinhaRepository.listar();
+	}
+	
+	@GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
+	public CozinhasXmlWrapper listarXml() {
+		return new CozinhasXmlWrapper(cozinhaRepository.listar());
 	}
 
 	@GetMapping("/{cozinhaId}")
