b12a19bd50448a430670241ad59d7448f7104670
erich <erich@DESKTOP-HUIJ7D8>
2022-03-22 14:41:11 -0400

    4.19. Definindo o status da resposta HTTP com @ResponseStatus

--- src/main/java/com/erichiroshi/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/erichiroshi/algafood/api/controller/CozinhaController.java
@@ -3,10 +3,12 @@ package com.erichiroshi.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.erichiroshi.algafood.api.model.CozinhasXmlWrapper;
@@ -20,8 +22,9 @@ public class CozinhaController {
 	@Autowired
 	private CozinhaRepository cozinhaRepository;
 
+	@ResponseStatus(HttpStatus.BANDWIDTH_LIMIT_EXCEEDED)
 	@GetMapping
-	public List<Cozinha> listar1() {
+	public List<Cozinha> listar() {
 		return cozinhaRepository.listar();
 	}
 	
