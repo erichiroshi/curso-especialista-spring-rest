0d7e7f2a5498a5803e6a62326d8f1139489d01ca
erich <erich@DESKTOP-HUIJ7D8>
2022-03-22 20:24:04 -0400

    4.28. Refatorando a exclusão de cozinhas para usar domain services

--- src/main/java/com/erichiroshi/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/erichiroshi/algafood/api/controller/CozinhaController.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +19,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.erichiroshi.algafood.api.model.CozinhasXmlWrapper;
+import com.erichiroshi.algafood.domain.exception.EntidadeEmUsoException;
+import com.erichiroshi.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.erichiroshi.algafood.domain.model.Cozinha;
 import com.erichiroshi.algafood.domain.repository.CozinhaRepository;
 import com.erichiroshi.algafood.domain.service.CadastroCozinhaService;
@@ -74,13 +75,13 @@ public class CozinhaController {
 	@DeleteMapping("/{cozinhaId}")
 	public ResponseEntity<Void> deletar(@PathVariable Long cozinhaId) {
 		try {
-			Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
-			if (cozinha != null) {
-				cozinhaRepository.remover(cozinha);
-				return ResponseEntity.noContent().build();
-			}
+			cadastroCozinha.excluir(cozinhaId);
+			return ResponseEntity.noContent().build();
+
+		} catch (EntidadeNaoEncontradaException e) {
 			return ResponseEntity.notFound().build();
-		}catch (DataIntegrityViolationException e) {
+
+		} catch (EntidadeEmUsoException e) {
 			return ResponseEntity.status(HttpStatus.CONFLICT).build();
 		}
 	}
