2ffdb0d814d68a75874704bf2f4df4f6e6ec3680
erich <erich@DESKTOP-HUIJ7D8>
2022-03-24 09:57:20 -0400

    5.4. Refatorando o código do projeto para usar o repositório do SDJ

--- src/main/java/com/erichiroshi/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/erichiroshi/algafood/api/controller/CozinhaController.java
@@ -2,6 +2,7 @@ package com.erichiroshi.algafood.api.controller;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,14 +36,14 @@ public class CozinhaController {
 
 	@GetMapping
 	public List<Cozinha> listar() {
-		return cozinhaRepository.listar();
+		return cozinhaRepository.findAll();
 	}
 
 	@GetMapping("/{cozinhaId}")
 	public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
-		Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
-		if (cozinha != null) {
-			return ResponseEntity.ok(cozinha);
+		Optional<Cozinha> cozinha = cozinhaRepository.findById(cozinhaId);
+		if (cozinha.isPresent()) {
+			return ResponseEntity.ok(cozinha.get());
 		}
 		return ResponseEntity.notFound().build();
 	}
@@ -56,11 +57,11 @@ public class CozinhaController {
 
 	@PutMapping("/{cozinhaId}")
 	public ResponseEntity<Cozinha> atualizar(@PathVariable Long cozinhaId, @RequestBody Cozinha cozinha) {
-		Cozinha cozinhaAtual = cozinhaRepository.buscar(cozinhaId);
-		if (cozinhaAtual != null) {
-			BeanUtils.copyProperties(cozinha, cozinhaAtual, "id");
-			cozinhaAtual = cadastroCozinha.salvar(cozinhaAtual);
-			return ResponseEntity.ok(cozinhaAtual);
+		Optional<Cozinha> cozinhaAtual = cozinhaRepository.findById(cozinhaId);
+		if (cozinhaAtual.isPresent()) {
+			BeanUtils.copyProperties(cozinha, cozinhaAtual.get(), "id");
+			Cozinha cozinhaSalva = cadastroCozinha.salvar(cozinhaAtual.get());
+			return ResponseEntity.ok(cozinhaSalva);
 		}
 		return ResponseEntity.notFound().build();
 	}
--- src/main/java/com/erichiroshi/algafood/api/controller/TesteController.java
+++ src/main/java/com/erichiroshi/algafood/api/controller/TesteController.java
@@ -1,27 +1,19 @@
 package com.erichiroshi.algafood.api.controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.erichiroshi.algafood.domain.model.Cozinha;
-import com.erichiroshi.algafood.domain.repository.CozinhaRepository;
 
 @RestController
 @RequestMapping("/teste")
 public class TesteController {
 
-	@Autowired
-	private CozinhaRepository cozinhaRepository;
-	
-	@GetMapping("/cozinhas/por-nome")
-	public List<Cozinha> listarPorNome(@RequestParam String nome){
-		List<Cozinha> list = cozinhaRepository.consultaPorNome(nome);
-		return list;
-	}
-	
+//	@Autowired
+//	private CozinhaRepository cozinhaRepository;
+//	
+//	@GetMapping("/cozinhas/por-nome")
+//	public List<Cozinha> listarPorNome(@RequestParam String nome){
+//		List<Cozinha> list = cozinhaRepository.consultaPorNome(nome);
+//		return list;
+//	}
+//	
 }
