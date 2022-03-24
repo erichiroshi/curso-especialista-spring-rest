c06a8b7a717fe0c0e566e4ede0f34a14c46c341e
erich <erich@DESKTOP-HUIJ7D8>
2022-03-24 09:14:37 -0400

    5.1. Implementando consultas JPQL em repositórios (consulta por nome)

--- src/main/java/com/erichiroshi/algafood/domain/repository/CozinhaRepository.java
+++ src/main/java/com/erichiroshi/algafood/domain/repository/CozinhaRepository.java
@@ -7,6 +7,7 @@ import com.erichiroshi.algafood.domain.model.Cozinha;
 public interface CozinhaRepository {
 
 	List<Cozinha> listar();
+	List<Cozinha> consultaPorNome(String nome);
 	Cozinha buscar(Long id);
 	Cozinha salvar(Cozinha cozinha);
 	void remover(Long id);
--- src/main/java/com/erichiroshi/algafood/infrastructure/repository/CozinhaRepositoryImpl.java
+++ src/main/java/com/erichiroshi/algafood/infrastructure/repository/CozinhaRepositoryImpl.java
@@ -6,13 +6,13 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.erichiroshi.algafood.domain.model.Cozinha;
 import com.erichiroshi.algafood.domain.repository.CozinhaRepository;
 
-@Component
+@Repository
 public class CozinhaRepositoryImpl implements CozinhaRepository {
 
 	@PersistenceContext
@@ -23,6 +23,13 @@ public class CozinhaRepositoryImpl implements CozinhaRepository {
 		return entityManager.createQuery("FROM Cozinha", Cozinha.class).getResultList();
 	}
 
+	@Override
+	public List<Cozinha> consultaPorNome(String nome) {
+		return entityManager.createQuery("from Cozinha where nome like :nome", Cozinha.class)
+					.setParameter("nome", "%" + nome + "%")
+					.getResultList();
+	}
+	
 	@Override
 	public Cozinha buscar(Long id) {
 		return entityManager.find(Cozinha.class, id);
@@ -43,5 +50,7 @@ public class CozinhaRepositoryImpl implements CozinhaRepository {
 		}
 		entityManager.remove(cozinha);
 	}
+
+
 
 }
