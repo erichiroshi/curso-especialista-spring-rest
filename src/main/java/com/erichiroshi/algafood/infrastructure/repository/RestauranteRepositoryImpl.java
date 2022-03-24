3d003acaaca9b35c2fdcc0d3bb7dcbcc14b2d6e9
erich <erich@DESKTOP-HUIJ7D8>
2022-03-24 15:26:48 -0400

    5.11. Implementando um repositório SDJ customizado

---
+++ src/main/java/com/erichiroshi/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -0,0 +1,31 @@
+package com.erichiroshi.algafood.infrastructure.repository;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository;
+
+import com.erichiroshi.algafood.domain.model.Restaurante;
+import com.erichiroshi.algafood.domain.repository.RestauranteRepositoryQuery;
+
+@Repository
+public class RestauranteRepositoryImpl implements RestauranteRepositoryQuery {
+
+	@PersistenceContext
+	private EntityManager manager;
+
+	@Override
+	public List<Restaurante> find(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
+
+		var jpql = "from Restaurante where nome like :nome and taxaFrete between :taxaInicial and :taxaFinal";
+
+		return manager.createQuery(jpql, Restaurante.class)
+				.setParameter("nome", "%" + nome + "%")
+				.setParameter("taxaInicial", taxaFreteInicial)
+				.setParameter("taxaFinal", taxaFreteFinal)
+				.getResultList();
+	}
+}
