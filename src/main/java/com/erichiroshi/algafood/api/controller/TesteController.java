8c5a2c2797712a80f52f12c5a5797cfcf10bfd35
erich <erich@DESKTOP-HUIJ7D8>
2022-03-24 20:07:16 -0400

    5.17. Implementando Specifications com SDJ

--- src/main/java/com/erichiroshi/algafood/api/controller/TesteController.java
+++ src/main/java/com/erichiroshi/algafood/api/controller/TesteController.java
@@ -14,6 +14,8 @@ import com.erichiroshi.algafood.domain.model.Cozinha;
 import com.erichiroshi.algafood.domain.model.Restaurante;
 import com.erichiroshi.algafood.domain.repository.CozinhaRepository;
 import com.erichiroshi.algafood.domain.repository.RestauranteRepository;
+import com.erichiroshi.algafood.infrastructure.repository.spec.RestauranteComFreteGratisSpec;
+import com.erichiroshi.algafood.infrastructure.repository.spec.RestauranteComNomeSemelhanteSpec;
 
 @RestController
 @RequestMapping("/teste")
@@ -74,5 +76,13 @@ public class TesteController {
 	@GetMapping("/restaurantes/count-por-cozinhaId")
 	public int restauranteCountCozinhaId(Long cozinhaId) {
 		return restauranteRepository.countByCozinhaId(cozinhaId);
+	}
+	
+	@GetMapping("/restaurantes/com-frete-gratis")
+	public List<Restaurante> restaurantesComFreteGratis(String nome) {
+		var comFreteGratis = new RestauranteComFreteGratisSpec();
+		var comNomeSemelhante = new RestauranteComNomeSemelhanteSpec(nome);
+		
+		return restauranteRepository.findAll(comNomeSemelhante.and(comFreteGratis));
 	}
 }
