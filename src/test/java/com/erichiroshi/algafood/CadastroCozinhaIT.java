7468b4545a1e9f3a4863ef5c517c1d2c1c8347e1
erichiroshi <erichiroshi@gmail.com>
2022-10-03 14:06:25 -0400

    10.10. Criando um método para fazer setup dos testes (@BeforeEach)

--- src/test/java/com/erichiroshi/algafood/CadastroCozinhaIT.java
+++ src/test/java/com/erichiroshi/algafood/CadastroCozinhaIT.java
@@ -1,9 +1,9 @@
 package com.erichiroshi.algafood;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.hasItems;
 import static org.hamcrest.Matchers.hasSize;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
@@ -18,32 +18,30 @@ public class CadastroCozinhaIT {
 	@LocalServerPort
 	private int port;
 	
+	@BeforeEach
+	public void setUp() {
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+		RestAssured.port = port;
+		RestAssured.basePath = "/cozinhas";
+	}
+	
 	@Test
 	public void deveRetornarStatus200_QuandoConsultarCozinhas() {
-		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
-		
 		given()
-			.basePath("/cozinhas")
-			.port(port)
 			.accept(ContentType.JSON)
 		.when()
 			.get()
 		.then()
 			.statusCode(HttpStatus.OK.value());
 	}
-	
+
 	@Test
 	public void deveConter4Cozinhas_QuandoConsultarCozinhas() {
 		given()
-		.basePath("/cozinhas")
-		.port(port)
-		.accept(ContentType.JSON)
-	.when()
-		.get()
-	.then()
-		.body("", hasSize(4))
-		.body("nome", hasItems("Indiana", "Tailandesa"))
-		;
+			.accept(ContentType.JSON)
+		.when()
+			.get()
+		.then()
+			.body("", hasSize(4));
 	}
-	
 }
