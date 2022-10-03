cca745b47d0347dbf1496a48d1e9d40eb7e629a6
erichiroshi <erichiroshi@gmail.com>
2022-10-03 10:09:34 -0400

    10.5. Desafio: escrevendo testes de integração

--- src/test/java/com/erichiroshi/algafood/CadastroCozinhaIntegrationTests.java
+++ src/test/java/com/erichiroshi/algafood/CadastroCozinhaIntegrationTests.java
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.erichiroshi.algafood.domain.exception.CozinhaNaoEncontradaException;
+import com.erichiroshi.algafood.domain.exception.EntidadeEmUsoException;
 import com.erichiroshi.algafood.domain.model.Cozinha;
 import com.erichiroshi.algafood.domain.service.CadastroCozinhaService;
 
@@ -45,4 +47,43 @@ public class CadastroCozinhaIntegrationTests {
 		assertThat(erroEsperado).isNotNull();
 	}
 
+	@Test
+	public void deveFalhar_QuandoExcluirCozinhaEmUso() {
+
+		EntidadeEmUsoException erroEsperado = 
+				Assertions.assertThrows(EntidadeEmUsoException.class, () -> {
+					cadastroCozinha.excluir(1L);
+		});
+
+		assertThat(erroEsperado).isNotNull();
+	}
+	
+	@Test
+	public void deveFalhar_QuandoExcluirCozinhaInexistente() {
+	
+		CozinhaNaoEncontradaException erroEsperado = 
+				Assertions.assertThrows(CozinhaNaoEncontradaException.class, () -> {
+					cadastroCozinha.excluir(100L);
+		});
+
+		assertThat(erroEsperado).isNotNull();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
