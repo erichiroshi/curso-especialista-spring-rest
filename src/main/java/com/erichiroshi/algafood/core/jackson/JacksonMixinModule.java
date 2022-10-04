a13599b59ca8892e7756bffd527dc6e614a8e8ad
erichiroshi <erichiroshi@gmail.com>
2022-10-04 14:54:12 -0400

    11.12. Refatorando e criando um assembler de DTO

--- src/main/java/com/erichiroshi/algafood/core/jackson/JacksonMixinModule.java
+++ src/main/java/com/erichiroshi/algafood/core/jackson/JacksonMixinModule.java
@@ -4,10 +4,8 @@ import org.springframework.stereotype.Component;
 
 import com.erichiroshi.algafood.api.model.mixin.CidadeMixin;
 import com.erichiroshi.algafood.api.model.mixin.CozinhaMixin;
-import com.erichiroshi.algafood.api.model.mixin.RestauranteMixin;
 import com.erichiroshi.algafood.domain.model.Cidade;
 import com.erichiroshi.algafood.domain.model.Cozinha;
-import com.erichiroshi.algafood.domain.model.Restaurante;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 
 @Component
@@ -15,7 +13,6 @@ public class JacksonMixinModule extends SimpleModule {
 	private static final long serialVersionUID = 1L;
 
 	public JacksonMixinModule() {
-		setMixInAnnotation(Restaurante.class, RestauranteMixin.class);
 		setMixInAnnotation(Cidade.class, CidadeMixin.class);
 		setMixInAnnotation(Cozinha.class, CozinhaMixin.class);
 	}
