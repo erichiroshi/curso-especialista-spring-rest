511066a357845fbe2e9e1070c5c419dedc902316
erichiroshi <erichiroshi@gmail.com>
2022-10-05 09:52:47 -0400

    11.17. Mapeando para uma instância destino (e não um tipo) com ModelMapper (método atualizar com modelMapper)

--- src/main/java/com/erichiroshi/algafood/api/assembler/RestauranteInputDisassembler.java
+++ src/main/java/com/erichiroshi/algafood/api/assembler/RestauranteInputDisassembler.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.erichiroshi.algafood.api.model.input.RestauranteInput;
+import com.erichiroshi.algafood.domain.model.Cozinha;
 import com.erichiroshi.algafood.domain.model.Restaurante;
 
 @Component
@@ -18,4 +19,12 @@ public class RestauranteInputDisassembler {
 		return modelMapper.map(restauranteInput, Restaurante.class);
 	}
 
+	public void copyToDomainObject(RestauranteInput restauranteInput, Restaurante restaurante) {
+
+//		Para evitar  org.hibernate.HibernateException: identifier of an instance of 
+//		com.erichiroshi.algafood.domain.model.Cozinha was altered from 1 to 2
+		restaurante.setCozinha(new Cozinha());
+		
+		modelMapper.map(restauranteInput, restaurante);
+	}
 }
