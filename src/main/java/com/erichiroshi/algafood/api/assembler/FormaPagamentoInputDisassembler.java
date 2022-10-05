5fa0b02f18217272ddaedf8de358c501176b85d9
erichiroshi <erichiroshi@gmail.com>
2022-10-05 14:04:19 -0400

    12.5. Desafio: implementando os endpoints de formas de pagamento

---
+++ src/main/java/com/erichiroshi/algafood/api/assembler/FormaPagamentoInputDisassembler.java
@@ -0,0 +1,23 @@
+package com.erichiroshi.algafood.api.assembler;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.erichiroshi.algafood.api.model.input.FormaPagamentoInput;
+import com.erichiroshi.algafood.domain.model.FormaPagamento;
+
+@Component
+public class FormaPagamentoInputDisassembler {
+
+    @Autowired
+    private ModelMapper modelMapper;
+    
+    public FormaPagamento toDomainObject(FormaPagamentoInput formaPagamentoInput) {
+        return modelMapper.map(formaPagamentoInput, FormaPagamento.class);
+    }
+    
+    public void copyToDomainObject(FormaPagamentoInput formaPagamentoInput, FormaPagamento formaPagamento) {
+        modelMapper.map(formaPagamentoInput, formaPagamento);
+    }   
+}       
