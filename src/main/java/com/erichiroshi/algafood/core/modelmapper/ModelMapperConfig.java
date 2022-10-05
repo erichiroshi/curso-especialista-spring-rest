921ffb6bd13f70278937be256a86288298b3888b
erichiroshi <erichiroshi@gmail.com>
2022-10-05 09:30:57 -0400

    11.16. Customizando o mapeamento de propriedades com ModelMapper

--- src/main/java/com/erichiroshi/algafood/core/modelmapper/ModelMapperConfig.java
+++ src/main/java/com/erichiroshi/algafood/core/modelmapper/ModelMapperConfig.java
@@ -4,11 +4,20 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.erichiroshi.algafood.api.model.RestauranteModel;
+import com.erichiroshi.algafood.domain.model.Restaurante;
+
 @Configuration
 public class ModelMapperConfig {
 
 	@Bean
 	public ModelMapper modelMapper() {
-		return new ModelMapper();
+		
+		ModelMapper modelMapper = new ModelMapper();
+		
+		modelMapper.createTypeMap(Restaurante.class, RestauranteModel.class)
+			.addMapping(Restaurante::getTaxaFrete, RestauranteModel::setPrecoFrete);
+		
+		return modelMapper;
 	}
 }
