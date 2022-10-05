e70fea27a01872bb2bd5a9a71a54e0403e492cbf
erichiroshi <erichiroshi@gmail.com>
2022-10-05 09:16:09 -0400

    11.14. Adicionando e usando o ModelMapper

--- src/main/java/com/erichiroshi/algafood/api/assembler/RestauranteInputDisassembler.java
+++ src/main/java/com/erichiroshi/algafood/api/assembler/RestauranteInputDisassembler.java
@@ -1,25 +1,21 @@
 package com.erichiroshi.algafood.api.assembler;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.erichiroshi.algafood.api.model.input.RestauranteInput;
-import com.erichiroshi.algafood.domain.model.Cozinha;
 import com.erichiroshi.algafood.domain.model.Restaurante;
 
 @Component
 public class RestauranteInputDisassembler {
 
-    public Restaurante toDomainObject(RestauranteInput restauranteInput) {
-        Restaurante restaurante = new Restaurante();
-        restaurante.setNome(restauranteInput.getNome());
-        restaurante.setTaxaFrete(restauranteInput.getTaxaFrete());
-        
-        Cozinha cozinha = new Cozinha();
-        cozinha.setId(restauranteInput.getCozinha().getId());
-        
-        restaurante.setCozinha(cozinha);
-        
-        return restaurante;
-    }
-    
+	@Autowired
+	private ModelMapper modelMapper;
+
+	public Restaurante toDomainObject(RestauranteInput restauranteInput) {
+
+		return modelMapper.map(restauranteInput, Restaurante.class);
+	}
+
 }
