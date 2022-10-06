2c8aa1f593c24b3294ac6ec548940937116e3431
erichiroshi <erichiroshi@gmail.com>
2022-10-06 10:14:37 -0400

    12.11. Implementando regra de negócio para evitar usuários com e-mails duplicados

--- src/main/java/com/erichiroshi/algafood/domain/repository/UsuarioRepository.java
+++ src/main/java/com/erichiroshi/algafood/domain/repository/UsuarioRepository.java
@@ -1,10 +1,14 @@
 package com.erichiroshi.algafood.domain.repository;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Repository;
 
 import com.erichiroshi.algafood.domain.model.Usuario;
 
 @Repository
 public interface UsuarioRepository extends CustomJpaRepository<Usuario, Long> {
+
+	Optional<Usuario> findByEmail(String email);
 
 }
