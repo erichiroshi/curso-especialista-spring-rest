9aed9bcec0bba43aca4f59b16e2baef1f6e6ef91
erichiroshi <erichiroshi@gmail.com>
2022-10-06 15:01:26 -0400

    12.17. Desafio: implementando endpoints de associação de usuários responsáveis com restaurantes

--- src/main/java/com/erichiroshi/algafood/domain/service/CadastroRestauranteService.java
+++ src/main/java/com/erichiroshi/algafood/domain/service/CadastroRestauranteService.java
@@ -9,6 +9,7 @@ import com.erichiroshi.algafood.domain.model.Cidade;
 import com.erichiroshi.algafood.domain.model.Cozinha;
 import com.erichiroshi.algafood.domain.model.FormaPagamento;
 import com.erichiroshi.algafood.domain.model.Restaurante;
+import com.erichiroshi.algafood.domain.model.Usuario;
 import com.erichiroshi.algafood.domain.repository.RestauranteRepository;
 
 @Service
@@ -25,6 +26,9 @@ public class CadastroRestauranteService {
 	
 	@Autowired
 	private CadastroFormaPagamentoService cadastroFormaPagamento;
+	
+	@Autowired
+	private CadastroUsuarioService cadastroUsuario;
 	
 	@Transactional
 	public Restaurante salvar(Restaurante restaurante) {
@@ -82,6 +86,22 @@ public class CadastroRestauranteService {
 	    Restaurante restauranteAtual = buscarOuFalhar(restauranteId);
 	    
 	    restauranteAtual.fechar();
+	}
+	
+	@Transactional
+	public void desassociarResponsavel(Long restauranteId, Long usuarioId) {
+	    Restaurante restaurante = buscarOuFalhar(restauranteId);
+	    Usuario usuario = cadastroUsuario.buscarOuFalhar(usuarioId);
+	    
+	    restaurante.removerResponsavel(usuario);
+	}
+
+	@Transactional
+	public void associarResponsavel(Long restauranteId, Long usuarioId) {
+	    Restaurante restaurante = buscarOuFalhar(restauranteId);
+	    Usuario usuario = cadastroUsuario.buscarOuFalhar(usuarioId);
+	    
+	    restaurante.adicionarResponsavel(usuario);
 	}
 	
 	public Restaurante buscarOuFalhar(Long restauranteId) {
